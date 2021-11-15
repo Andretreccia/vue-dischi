3d4ab7a7f04e48803f1daa61367d52ffe55db448
Andretreccia <andreabtreccia@gmail.com>
2021-11-15 11:44:53 +0100

    Initial commit
    Installato Vue e installato e importato bootstrap

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
