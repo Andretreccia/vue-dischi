cb92e38194fc8706a1168945a5654d68feac1798
Andretreccia <andreabtreccia@gmail.com>
2021-11-15 13:59:43 +0100

    add elements to display and a bit of style

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,7 +2,22 @@
   <main id="site-main">
     <div class="container">
       <div class="row">
-        <div class="card" v-for="music in musics" :key="music.ident"></div>
+        <div class="col col-2" v-for="music in musics" :key="music.ident">
+          <div class="card p-3">
+            <div class="image">
+              <img class="img-fluid" :src="music.poster" alt="" />
+            </div>
+            <div class="title">
+              <h4>{{ music.title }}</h4>
+            </div>
+            <div class="author">
+              <span>{{ music.author }}</span>
+            </div>
+            <div class="year">
+              <span> {{ music.year }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -32,5 +47,26 @@ export default {
 <style lang="scss">
 #site-main {
   background: #1e2d3b;
+  .row {
+    padding-top: 7rem;
+    .col {
+      margin-top: 1rem;
+      .card {
+        height: 320px;
+        background: rgba(255, 255, 255, 0.088);
+        text-align: center;
+        .image {
+          margin-bottom: 1rem;
+        }
+        .title {
+          color: white;
+        }
+        .author,
+        .year {
+          color: gray;
+        }
+      }
+    }
+  }
 }
 </style>
